42060966296bf3e99f9f1d3c431f5099cb0f337f
Autumn <auctumnus@pm.me>
2024-07-02 15:50:07 -0500

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, crane, fenix }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+	  pkgs = nixpkgs.legacyPackages.${system};
+
+          craneLib = (crane.mkLib pkgs).overrideToolchain 
+	  	(fenix.packages.${system}.fromToolchainFile {
+	  		file = ./rust-toolchain.toml;
+			sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+	  	});
+
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+          };
+
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          my-crate = craneLib.buildPackage (commonArgs // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          });
+        in
+        with pkgs;
+        {
+          checks = {
+            inherit my-crate;
+          };
+          packages.default = my-crate;
+
+          devShells.default = craneLib.devShell {
+            checks = self.checks.${system};
+          };
+        }
+      );
+}
